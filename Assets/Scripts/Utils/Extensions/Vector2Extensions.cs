eb3690f2231bcfd2ee05393b4008b4b5998f6b90
Kevin Krol <Krol.kevin@gmail.com>
2016-04-19 11:07:41 +0200

    Added Vector2 extensions

---
+++ Assets/Scripts/Utils/Extensions/Vector2Extensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public static class Vector2Extensions
+{
+	public static bool AlmostEquals(this Vector2 lhs, Vector2 rhs, float epsilon)
+	{
+		return (Math.Abs(lhs.x) - Math.Abs(rhs.x) <= epsilon) && (Math.Abs(lhs.y) - Math.Abs(rhs.y) <= epsilon);
+	}
+}
