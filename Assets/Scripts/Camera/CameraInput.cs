26da723d8b0fdbcd62bd66d7a20ade33aec82439
Ted-Laptop-New <meatkroket@hotmail.com>
2016-04-21 11:10:14 +0200

    Fixed camera rotation on mobile

--- Assets/Scripts/Camera/CameraInput.cs
+++ Assets/Scripts/Camera/CameraInput.cs
@@ -38,14 +38,62 @@ public class CameraInput
 			
 		#endif
 
-		Touch[] touches = Input.touches;
-		if (touches.Length > 1)
+		if(Application.isMobilePlatform)
 		{
-			if (touches [0].phase == TouchPhase.Began || touches [1].phase == TouchPhase.Began) 
+			Touch[] touches = Input.touches;
+			if (touches.Length > 1)
+			{
+				if (touches [0].phase == TouchPhase.Began || touches [1].phase == TouchPhase.Began) 
+				{
+					dist = MathHelper.dis2 (touches [0].position.x, touches [0].position.y, touches [1].position.x, touches [1].position.y);
+					isMoving = true;
+				}
+				else if (touches [0].phase != TouchPhase.Began && touches [1].phase != TouchPhase.Began && isMoving) 
+				{
+					float currentDist = MathHelper.dis2 (touches [0].position.x, touches [0].position.y, touches [1].position.x, touches [1].position.y);
+					deltaZoom = (currentDist - dist);
+					dist = currentDist;
+				}
+			} 
+			else if (touches.Length == 1) 
+			{
+				if (touches [0].phase == TouchPhase.Began)
+				{
+					if (!isTouchingPlayer (new Vector2 (touches [0].position.x, touches [0].position.y)))
+					{
+						prevX = (touches [0].position.x / Screen.width) * 1920f;
+						prevY = (touches [0].position.y / Screen.height) * 1080f;
+						isMoving = true;
+					} 
+					else
+					{
+						isMoving = false;
+					}
+				}
+				else if(touches [0].phase == TouchPhase.Moved && isMoving)
+				{
+					float currentX = (touches[0].position.x / Screen.width) * 1920f;
+					float currentY = (touches[0].position.y / Screen.height) * 1080f;
+					moveX = currentX - prevX;
+					moveY = currentY - prevY;
+					prevX = currentX;
+					prevY = currentY;
+				}
+				else if(touches [0].phase == TouchPhase.Ended || touches [0].phase == TouchPhase.Canceled)
+				{
+					isMoving = false;
+				}
+			}
+		}
+		else
+		{
+			deltaZoom = Input.mouseScrollDelta.y * 20f;
+			if(Input.GetKeyDown(KeyCode.Mouse0))
 			{
 				if(!isTouchingPlayer(new Vector2(Input.mousePosition.x, Input.mousePosition.y)))
 				{
-					dist = MathHelper.dis2 (touches [0].position.x, touches [0].position.y, touches [1].position.x, touches [1].position.y);
+					prevX = (Input.mousePosition.x / Screen.width) * 1920f;
+					prevY = (Input.mousePosition.y / Screen.height) * 1080f;
 					isMoving = true;
 				}
 				else
@@ -53,53 +101,19 @@ public class CameraInput
 					isMoving = false;
 				}
 			}
-			else if (touches [0].phase != TouchPhase.Began && touches [1].phase != TouchPhase.Began && isMoving) 
+			else if(Input.GetKey(KeyCode.Mouse0) && isMoving)
 			{
-				float currentDist = MathHelper.dis2 (touches [0].position.x, touches [0].position.y, touches [1].position.x, touches [1].position.y);
-				deltaZoom = (currentDist - dist);
-				dist = currentDist;
-			}
-		} 
-		else if (touches.Length == 1) 
-		{
-			if (touches [0].phase == TouchPhase.Began)
-			{
-				prevX = (touches[0].position.x / Screen.width) * 1920f;
-				prevY = (touches[0].position.y / Screen.height) * 1080f;
-			}
-			else if(touches [0].phase == TouchPhase.Moved)
-			{
-				float currentX = (touches[0].position.x / Screen.width) * 1920f;
-				float currentY = (touches[0].position.y / Screen.height) * 1080f;
+				float currentX = (Input.mousePosition.x / Screen.width) * 1920f;
+				float currentY = (Input.mousePosition.y / Screen.height) * 1080f;
 				moveX = currentX - prevX;
 				moveY = currentY - prevY;
 				prevX = currentX;
 				prevY = currentY;
 			}
-		}
-
-		deltaZoom = Input.mouseScrollDelta.y * 20f;
-		if(Input.GetKeyDown(KeyCode.Mouse0))
-		{
-			if(!isTouchingPlayer(new Vector2(Input.mousePosition.x, Input.mousePosition.y)))
-			{
-				prevX = (Input.mousePosition.x / Screen.width) * 1920f;
-				prevY = (Input.mousePosition.y / Screen.height) * 1080f;
-				isMoving = true;
-			}
-			else
+			else if(Input.GetKeyUp(KeyCode.Mouse0))
 			{
 				isMoving = false;
 			}
-		}
-		else if(Input.GetKey(KeyCode.Mouse0) && isMoving)
-		{
-			float currentX = (Input.mousePosition.x / Screen.width) * 1920f;
-			float currentY = (Input.mousePosition.y / Screen.height) * 1080f;
-			moveX = currentX - prevX;
-			moveY = currentY - prevY;
-			prevX = currentX;
-			prevY = currentY;
 		}
 	}
 
