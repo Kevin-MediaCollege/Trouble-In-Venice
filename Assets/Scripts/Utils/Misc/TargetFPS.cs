06e63afedbee1a1366d745f254d64dce729d926c
Kevin Krol <Krol.kevin@gmail.com>
2016-04-26 15:05:35 +0200

    Performance optimizations

---
+++ Assets/Scripts/Utils/Misc/TargetFPS.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetFPS : MonoBehaviour
+{
+	[SerializeField] private int targetFPS;
+
+	protected void Awake()
+	{
+		Application.targetFrameRate = targetFPS;
+	}
+}
