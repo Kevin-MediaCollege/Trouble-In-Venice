c4c392562d89480cbfc5c53ee7bde5c4d63eaa58
Ted-Laptop-New <meatkroket@hotmail.com>
2016-04-26 13:11:51 +0200

    Fixed swiping on mobile

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerInput : MonoBehaviour
 {
@@ -27,21 +28,37 @@ public class PlayerInput : MonoBehaviour
 		movement = GetComponent<EntityMovement>();
 	}
 
-	private GridNode GetSwipeTarget()
+	private Vector2 GetSwipeTarget()
 	{
-		Vector2 screenPoint1 = Camera.main.WorldToScreenPoint(nodeTracker.CurrentNode.Position);
+		Vector2 current = Camera.main.WorldToScreenPoint(MathHelper.XYtoXZ(nodeTracker.CurrentNode.Position));
+		float swipeRotation = MathHelper.PointToRotation (swipeHandle.StartPosition, swipeHandle.LastPosition);
 
-		Vector2 swipePoint1 = swipeHandle.StartPosition;
-		Vector2 swipePoint2 = swipeHandle.LastPosition;
-		
-		foreach(GridNode connection in nodeTracker.CurrentNode.Connections)
+		int x, y, i;
+		float closest = 80f;
+		Vector2 closestNode = Vector2.zero;
+
+		for(i = 0; i < 4; i++)
 		{
-			Vector2 screenPoint2 = Camera.main.WorldToScreenPoint(connection.Position);
-			
+			x = i == 1 ? 1 : i == 3 ? -1 : 0;
+			y = i == 0 ? 1 : i == 2 ? -1 : 0;
 
+			Vector2 neighbour = Camera.main.WorldToScreenPoint(MathHelper.XYtoXZ(nodeTracker.CurrentNode.Position + new Vector2(x, y)));
+			float diff = GetRotationDifference (swipeRotation, MathHelper.PointToRotation (current, neighbour));
+			
+			if(Mathf.Abs(diff) < Mathf.Abs(closest) && GridUtils.GetNodeAt(nodeTracker.CurrentNode.GridPosition + new Vector2(x, y)) != null)
+			{
+				closest = diff;
+				closestNode = new Vector2 (x, y);
+			}
 		}
 
-		return null;
+		return closestNode;
+	}
+
+	private float GetRotationDifference(float _a, float _b)
+	{
+		_b += _b > _a + 180f ? -360f : _b < _a - 180f ? 360f : 0f;
+		return _a - _b;
 	}
 
 	private void OnSwipeBeganEvent(SwipeBeganEvent _evt)
@@ -65,7 +82,7 @@ public class PlayerInput : MonoBehaviour
 	{
 		if(_evt.Handle == swipeHandle)
 		{
-			GetSwipeTarget();
+			Vector2 node = GetSwipeTarget();
 			//movement.Move(nodeTracker.CurrentNode.GridPosition + GetSwipeTarget().GridPosition);
 			swipeHandle = null;
 		}
---
+++ Assets/Scripts/Utils/Math/GridMath.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridMath 
+{
+	public static Vector2 WorldToGrid(Vector3 _world)
+	{
+		return new Vector2(Mathf.Round((_world.x - 1.5f) * 0.33333f), Mathf.Round((_world.z - 1.5f) * 0.33333f));
+	}
+
+	public static Vector3 GridToWorld(Vector2 _grid)
+	{
+		return new Vector3((_grid.x * 3f) + 1.5f, 0f, (_grid.y * 3f) + 1.5f);
+	}
+}
