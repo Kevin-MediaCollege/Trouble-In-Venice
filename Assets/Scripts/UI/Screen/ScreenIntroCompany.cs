641c00b528cdddf1c5eda35e9bb1c21aac694386
ted-Thuis <meatkroket@hotmail.com>
2016-05-04 11:39:04 +0200

    Added intro screen

---
+++ Assets/Scripts/UI/Screen/ScreenIntroCompany.cs
@@ -0,0 +1,99 @@
+﻿using DG;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScreenIntroCompany : ScreenBase 
+{
+	public GoatPrint[] printList;
+	public CanvasGroup logoAlphaGroup;
+	public RectTransform logoRect;
+	public CanvasGroup overlayAlphaGroup;
+
+	public override void OnScreenEnter()
+	{
+		StartCoroutine("IntroAnimation");
+	}
+
+	private IEnumerator IntroAnimation()
+	{
+		//hide everything
+		overlayAlphaGroup.alpha = 0f;
+		logoAlphaGroup.alpha = 1f;
+		logoRect.sizeDelta = new Vector2 (0f, 0f);
+		for(int i = 0; i < 5; i++)
+		{
+			printList[i].alphaGroup.alpha = 0f;
+			printList[i].rect.sizeDelta = new Vector2(0f, 0f);
+		}
+
+		yield return new WaitForSeconds(0.2f);
+
+		//Fade in base logo
+		logoRect.DOSizeDelta(new Vector2(1380f, 155f), 0.6f).SetEase(Ease.OutElastic);
+
+		yield return new WaitForSeconds(0.5f);
+
+		//Fade print 1
+		printList[0].alphaGroup.alpha = 1f;
+		printList[0].rect.DOSizeDelta(new Vector2(196f, 204f), 0.3f).SetEase(Ease.OutBounce);
+
+		yield return new WaitForSeconds(0.1f);
+
+		//Fade print 2
+		printList[1].alphaGroup.alpha = 1f;
+		printList[1].rect.DOSizeDelta(new Vector2(196f, 204f), 0.3f).SetEase(Ease.OutBounce);
+
+		yield return new WaitForSeconds(0.1f);
+
+		//Fade print 3
+		printList[2].alphaGroup.alpha = 1f;
+		printList[2].rect.DOSizeDelta(new Vector2(196f, 204f), 0.3f).SetEase(Ease.OutBounce);
+
+		yield return new WaitForSeconds(0.1f);
+
+		//Fade print 4
+		printList[3].alphaGroup.alpha = 1f;
+		printList[3].rect.DOSizeDelta(new Vector2(196f, 204f), 0.3f).SetEase(Ease.OutBounce);
+
+		yield return new WaitForSeconds(0.1f);
+
+		//Fade print 5
+		printList[4].alphaGroup.alpha = 1f;
+		printList[4].rect.DOSizeDelta(new Vector2(196f, 204f), 0.3f).SetEase(Ease.OutBounce);
+
+		yield return new WaitForSeconds(0.3f);
+
+		//Fade out all prints except print 2
+		for(int i = 0; i < 5; i++)
+		{
+			if(i != 2)
+			{
+				printList[i].alphaGroup.DOFade(0f, 0.3f);
+			}
+		}
+
+		yield return new WaitForSeconds(1.4f);
+
+		//fadeout screen
+		overlayAlphaGroup.DOFade(1f, 0.6f);
+
+		yield return new WaitForSeconds(0.6f);
+
+		//TODO: switch to menu scene
+	}
+
+	public override string GetScreenName()
+	{
+		return "ScreenIntroCompany";
+	}
+}
+
+[System.Serializable]
+public class GoatPrint
+{
+	public Image image;
+	public CanvasGroup alphaGroup;
+	public RectTransform rect;
+}
