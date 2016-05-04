0a1e1f1391c8ee2735d1e823cfc8a17d8a68e592
ted-Thuis <meatkroket@hotmail.com>
2016-05-04 13:28:06 +0200

    Added menu structure

---
+++ Assets/Scripts/UI/Screen/ScreenCredits.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenCredits : ScreenBase 
+{
+	public override void OnScreenEnter()
+	{
+	}
+
+	public override IEnumerator OnScreenFadeout()
+	{
+		yield break;
+	}
+
+	public override void OnScreenExit()
+	{
+	}
+
+	public override string GetScreenName()
+	{
+		return "ScreenCredits";
+	}
+}
