58e30fef9bf6dbcc9452504d4cef4112782954e8
Kevin Krol <Krol.kevin@gmail.com>
2016-05-13 12:18:53 +0200

    Added base object for node objects

---
+++ Assets/Scripts/Object/NodeObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Utils;
+
+namespace Proeve
+{
+	[RequireComponent(typeof(EntityNodeTracker))]
+	public class NodeObject : MonoBehaviour
+	{
+		protected GridNode node;
+		
+		protected virtual void Awake()
+		{
+			node = GetComponent<EntityNodeTracker>().CurrentNode;
+		}
+
+		protected virtual void OnEnable()
+		{
+			node.onEntityEnteredEvent += OnEntityEntered;
+			node.onEntityLeftEvent += OnEntityLeft;
+		}
+
+		protected virtual void OnDisable()
+		{
+			node.onEntityEnteredEvent -= OnEntityEntered;
+			node.onEntityLeftEvent -= OnEntityLeft;
+		}
+
+		protected virtual void OnEntityEntered(Entity _entity)
+		{
+		}
+
+		protected virtual void OnEntityLeft(Entity _entity)
+		{
+		}
+	}
+}
