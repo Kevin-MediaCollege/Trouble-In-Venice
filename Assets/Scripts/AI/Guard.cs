39d7c16908c2e60a0e9e84ba7cd94c5612451e89
Kevin Krol <Krol.kevin@gmail.com>
2016-05-13 12:19:35 +0200

    Added guard AI

---
+++ Assets/Scripts/AI/Guard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utils;
+
+namespace Proeve
+{
+	public class Guard : NodeObject
+	{
+		[SerializeField] private Vector2 direction;
+
+		protected override void Awake()
+		{
+			GridNode own = GetComponent<EntityNodeTracker>().CurrentNode;
+			node = GridUtils.GetNodeAt(own.GridPosition + direction);
+		}
+
+		protected void OnDrawGizmosSelected()
+		{
+			Vector3 position = Vector3.zero;
+
+			if(node != null)
+			{
+				position = node.Position;
+			}
+			else
+			{
+				GridNode own = GetComponent<EntityNodeTracker>().CurrentNode;
+				position = GridUtils.GetNodeAt(own.GridPosition + direction).Position;
+			}
+
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawSphere(position, 0.1f);
+		}
+
+		protected override void OnEntityEntered(Entity _entity)
+		{
+			if(_entity.HasTag("Player"))
+			{
+				Debug.Log("Kill player");
+			}
+		}
+	}
+}
