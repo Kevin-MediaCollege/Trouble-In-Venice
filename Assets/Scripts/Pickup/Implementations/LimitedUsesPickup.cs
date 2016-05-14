fe72fd21998150a8e55abe769e93462adbfac176
Kevin Krol <Krol.kevin@gmail.com>
2016-05-14 18:17:41 +0200

    Added a pickup with limited uses

---
+++ Assets/Scripts/Pickup/Implementations/LimitedUsesPickup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Proeve
+{
+	public class LimitedUsesPickup : Pickup
+	{
+		[SerializeField] private int uses;
+
+		private int remaining;
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			remaining = uses;
+		}
+
+		protected override void OnActivate()
+		{
+			remaining--;
+
+			if(remaining <= 0)
+			{
+				node.Active = false;
+			}
+		}
+	}
+}
