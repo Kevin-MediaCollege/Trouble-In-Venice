757d4ab5ed39239ca01bd67c8228d0221bf09eff
Kevin Krol <Krol.kevin@gmail.com>
2016-05-26 14:06:02 +0200

    Player an enemies now always face the camera

---
+++ Assets/Scripts/Misc/FaceTowardsCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceTowardsCamera : MonoBehaviour
+{
+	protected void LateUpdate()
+	{
+		Vector3 cameraPosition = Camera.main.transform.position;
+		Vector3 targetPostition = new Vector3(cameraPosition.x, transform.position.y, cameraPosition.z);
+
+		transform.LookAt(targetPostition);
+	}
+}
