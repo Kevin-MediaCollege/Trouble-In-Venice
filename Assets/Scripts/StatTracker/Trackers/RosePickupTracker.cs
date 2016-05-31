0af18ce142458cb1b8d8e3b6e05463ab1ef399f7
Kevin Krol <Krol.kevin@gmail.com>
2016-05-31 12:42:27 +0200

    Num stars can now be saved

---
+++ Assets/Scripts/StatTracker/Trackers/RosePickupTracker.cs
@@ -0,0 +1,37 @@
+﻿using Utils;
+
+namespace Proeve
+{
+	/// <summary>
+	/// Rose pickup tracker, tracks whether or not the player has picked up the rose in a level.
+	/// </summary>
+	public class RosePickupTracker : ITracker<bool>
+	{
+		private bool hasRose;
+
+		public void OnEnable()
+		{
+			GlobalEvents.AddListener<RosePickedUpEvent>(OnRosePickedUpEvent);
+		}
+
+		public void OnDisable()
+		{
+			GlobalEvents.RemoveListener<RosePickedUpEvent>(OnRosePickedUpEvent);
+		}
+
+		public bool GetValue()
+		{
+			return hasRose;
+		}
+
+		object ITracker.GetValue()
+		{
+			return GetValue();
+		}
+
+		private void OnRosePickedUpEvent(RosePickedUpEvent evt)
+		{
+			hasRose = true;
+		}
+	}
+}
